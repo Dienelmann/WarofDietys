8630d8f2f65ad49af1e0c7e6e6237d5eb9b199a7
Dienelmann <121038970+Dienelmann@users.noreply.github.com>
2023-09-15 11:16:12 +0200

    grundoffline

---
+++ Assets/Scripts/OfflineProgress.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System;
+using UnityEditor.UIElements;
+using UnityEngine.UI;
+using static UnityEngine.UI.Text;
+
+public class OfflineProgress : MonoBehaviour
+{
+    public Text Exp;
+    public Text time;
+    
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("Last_Login"))
+        {
+            DateTime lastLogin = DateTime.Parse(PlayerPrefs.GetString("Last_Login"));
+
+            TimeSpan timeSpan = DateTime.Now - lastLogin;
+            
+            time.text = String.Format("{0} Days {1} Hours {2} Minutes {3} seconnds", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+            Exp.text = (int)timeSpan.TotalMinutes + ("Exp");
+        }
+        else
+        {
+            time.text = "Welcome to War of Dietys";
+            Exp.text = " ";
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetString("Last_Login", DateTime.Now.ToString());
+    }
+}
